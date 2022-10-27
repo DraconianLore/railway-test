d832c3544460326618b42954b9324b2992069d5d
Steven Wing <steven.wing82@gmail.com>
2022-10-27 10:29:42 -0700

    move host config?

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+Rails.config.hosts << "rrtest.up.railway.app"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
