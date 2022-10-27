42cfdf627d7743fc83700c811f09eff0933578ec
Steven Wing <steven.wing82@gmail.com>
2022-10-27 10:38:27 -0700

    stupid hosts crap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
+  config.hosts << "rrtest.up.railway.app"
   
   # Code is not reloaded between requests.
   config.cache_classes = true
