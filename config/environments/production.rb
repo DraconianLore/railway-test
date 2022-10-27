62ae7983ed0f0262b1d5b93c60ecc2f5385753ab
Steven Wing <steven.wing82@gmail.com>
2022-10-27 10:01:55 -0700

    remove bad host?" "

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-  config.hosts << "rails-production-7314.up.railway.app"
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
